342280d79655a8a2d25cdb951856a1a3e3d8040b
Igor Aragão <roginaldosemog@gmail.com>
2020-09-07 23:41:54 -0300

    Install nodemon & add post test function

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (request, response) => {
-    return response.send('Hello World');
+app.post('/users', (request, response) => {
+    const body = request.body;
+    const query = request.query;
+    const params = request.params;
+
+    console.log(body);
+    // console.log(query);
+    // console.log(params);
+
+    return response.json({
+        evento: 'Omnistack 11.0',
+        aluno: 'Igor Aragão'
+    });
 });
 
 app.listen(3333);
